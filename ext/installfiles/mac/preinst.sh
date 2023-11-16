92b83daebde645ce0f4fcfaba0a9713a8c9d1dc7
gkiserpong <gkiserpongit@gmail.com>
2023-11-16 13:23:27 +0700

    Update ext/installfiles/mac/preinst.sh

--- ext/installfiles/mac/preinst.sh
+++ ext/installfiles/mac/preinst.sh
@@ -3,7 +3,7 @@
 export PATH=/bin:/usr/bin:/sbin:/usr/sbin
 
 rm -f /tmp/zt1-gui-restart.tmp
-for i in `ps axuwww | tr -s ' ' ',' | grep -F '/Applications/ZeroTier,One.app' | grep -F -v grep | cut -d , -f 1,2 | xargs`; do
+for i in `ps axuwww | tr -s ' ' ',' | grep -F '/Applications/BackOne.app' | grep -F -v grep | cut -d , -f 1,2 | xargs`; do
 	u=`echo $i | cut -d , -f 1`
 	p=`echo $i | cut -d , -f 2`
 	if [ ! -z "$u" -a "0$p" -gt 0 ]; then
@@ -13,7 +13,7 @@ for i in `ps axuwww | tr -s ' ' ',' | grep -F '/Applications/ZeroTier,One.app' |
 		echo "$u" >>/tmp/zt1-gui-restart.tmp
 	fi
 done
-for i in `ps axuwww | tr -s ' ' ',' | grep -F '/Applications/ZeroTier.app' | grep -F -v grep | cut -d , -f 1,2 | xargs`; do
+for i in `ps axuwww | tr -s ' ' ',' | grep -F '/Applications/BackOne.app' | grep -F -v grep | cut -d , -f 1,2 | xargs`; do
 	u=`echo $i | cut -d , -f 1`
 	p=`echo $i | cut -d , -f 2`
 	if [ ! -z "$u" -a "0$p" -gt 0 ]; then
@@ -26,13 +26,13 @@ done
 chmod 0600 /tmp/zt1-gui-restart.tmp
 
 cd "/Applications"
-rm -rf "ZeroTier One.app"
-rm -rf "ZeroTier.app"
+rm -rf "BackOne.app"
+rm -rf "BackOne.app"
 
-if [ -d '/Library/Application Support/ZeroTier/One' ]; then
-	cd '/Library/Application Support/ZeroTier/One'
+if [ -d '/Library/Application Support/BackOne' ]; then
+	cd '/Library/Application Support/BackOne'
 	# ensure that file locking doesn't cause issues with replacing the binary
-	rm -f zerotier-one
+	rm -f backone
 	rm -f MacEthernetTapAgent
 fi
 
